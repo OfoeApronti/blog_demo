dd9126444126e23eef19c679b37f7c710fb37165
blow <ofoe apronti>
2025-12-16 06:27:51 +0000

    seeded data

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_16_005124) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_16_021937) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.integer "views", default: 0
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_16_005124) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
